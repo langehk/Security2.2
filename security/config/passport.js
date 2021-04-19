3437a9075c9fc1c8e702d9fa6a3200665c90ca0e
nadiaskau <nadiaskau@hotmail.com>
2021-04-19 15:05:18 +0200

    secret facebook tokens

--- security/config/passport.js
+++ security/config/passport.js
@@ -5,14 +5,14 @@ const userModel = require('../model/User');
 
 module.exports = function(passport) {
   passport.use(new FacebookStrategy({
-    clientID: "3015782695319609",
-    clientSecret: "3dc92e36b50defbabdd76bae6631b97e",
+    clientID: "insert here",
+    clientSecret: "insert here",
     callbackURL: "http://localhost:3000/users/auth/facebook/callback" //takes user to frontpage when loggedin
   },
   function(accessToken, refreshToken, profile, done) {
     const { name, id } = profile._json;
     //We don't have the need to save our user - and it should be find or save 
-    
+
 /*     const userData = {
       _id: id,
       name: name
